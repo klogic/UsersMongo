00d05e104afd62be3e7ef42fcbab2bd168c22cc5
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-06-09 16:58:31 +0700

    make sure connect db first by using `before` and `done()`

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', ()=> console.log('connect database working'))
-  .on('error', (error)=> console.warn('Warning', error))
+// before is mocha hook. doing this fist and only 1 time
+before((done)=>{
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', ()=> { done() })
+    .on('error', (error)=> console.warn('Warning', error))
+})
 
+// beforeEach is mocha hook too. but doing it everytime before test 
 beforeEach((done)=>{
   mongoose.connection.collections.users.drop(()=>{
     done();
