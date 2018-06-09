e587e83d1dcb6eacef38f6a0f88696a29cab8810
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-06-09 16:28:02 +0700

    use beforeEach for hook mocha before do test function

--- test/test_helper.js
+++ test/test_helper.js
@@ -4,3 +4,9 @@ mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
   .once('open', ()=> console.log('connect database working'))
   .on('error', (error)=> console.warn('Warning', error))
+
+beforeEach((done)=>{
+  mongoose.connection.collections.users.drop(()=>{
+    done();
+  })
+});
